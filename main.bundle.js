b65a32a6281ed4bbb7ac418aaf35794e3205f396
jscholar <jinruncai@gmail.com>
2018-05-14 18:31:46 -0700

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -20,14 +20,14 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /***/ "./src/app/about/about.component.css":
 /***/ (function(module, exports) {
 
-module.exports = "\r\np {\r\n  float: left;\r\n  white-space: pre;\r\n  width: 1200px;\r\n  font-size: 1.5em;\r\n  background-color: rgba(0,0,0,0.7);\r\n  margin-left: 10px;\r\n}\r\n\r\na {\r\n  text-decoration: none;\r\n  color: #b0cdd9;\r\n}\r\n\r\na:hover {\r\n  color: #83c1d9;\r\n  text-decoration: underline;\r\n}\r\n"
+module.exports = "p {\r\n  float: left;\r\n  white-space: pre;\r\n  width: 1200px;\r\n  font-size: 1.5em;\r\n  background-color: rgba(0,0,0,0.7);\r\n  margin-left: 10px;\r\n}\r\n\r\na {\r\n  text-decoration: none;\r\n  color: #b0cdd9;\r\n}\r\n\r\na:hover {\r\n  color: #83c1d9;\r\n  text-decoration: underline;\r\n}\r\n\r\n#container {\r\n  white-space: nowrap;\r\n  overflow: hidden;\r\n}\r\n"
 
 /***/ }),
 
 /***/ "./src/app/about/about.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<app-home></app-home>\r\n\r\n<div>\r\n    <p class=\"\">\r\n      Welcome.\r\n\r\n      Lantern Dragon Works is a small collection fictional of novels inspired by Japanese visual novels and themes.\r\n\r\n      All credit for creation, story-writing, and characters go to the sole author, deltaOG (long may he live).\r\n\r\n      For now, this website is still in its infancy, as are my development skills. Therefore, if you\r\n      would like a more complete experience, on a more worthy interface, please visit this <a href=\"https://lanterndragonworks.wordpress.com/\">WordPress page</a> for now.\r\n      Though be sure to come back and visit again when this project becomes awesome.\r\n\r\n      Regards, and enjoy your stay,\r\n\r\n      A lowly member of the Lantern Dragons Works team.\r\n    </p>\r\n</div>\r\n"
+module.exports = "<app-home></app-home>\r\n\r\n<div id=\"container\">\r\n    <p>\r\n      Welcome.\r\n\r\n      Lantern Dragon Works is a small collection fictional of novels inspired by Japanese visual novels and themes.\r\n\r\n      All credit for creation, story-writing, and characters go to the sole author, deltaOG (long may he live).\r\n\r\n      For now, this website is still in its infancy, as are my development skills. Therefore, if you\r\n      would like a more complete experience, on a more worthy interface, please visit this <a href=\"https://lanterndragonworks.wordpress.com/\">WordPress page</a> for now.\r\n      Though be sure to come back and visit again when this project becomes awesome.\r\n\r\n      Regards, and enjoy your stay,\r\n\r\n      A lowly member of the Lantern Dragons Works team.\r\n    </p>\r\n</div>\r\n"
 
 /***/ }),
 
@@ -271,7 +271,7 @@ var GalleryComponent = /** @class */ (function () {
 /***/ "./src/app/home/home.component.css":
 /***/ (function(module, exports) {
 
-module.exports = "\r\n\r\nh1 {\r\n  font-family: Merienda;\r\n  font-weight: 400;\r\n  color: #83c1d9;\r\n  -webkit-user-select: none;\r\n     -moz-user-select: none;\r\n      -ms-user-select: none;\r\n          user-select: none;\r\n  text-shadow: 1px 1px #2a4353;\r\n}\r\n\r\na {\r\n  text-decoration: none;\r\n}\r\n\r\n.menu {\r\n  margin: 20px;\r\n  padding-left: 10px;\r\n  font-size: 4em;\r\n  width: 400px;\r\n  background: rgba(169, 169, 236, 0.2);\r\n}\r\n\r\n.main-menu nav{\r\n  margin-top: 50px;\r\n  width: -webkit-fit-content;\r\n  width: -moz-fit-content;\r\n  width: fit-content;\r\n  list-style-type: none;\r\n  white-space: nowrap;\r\n  float: left;\r\n  overflow: hidden;\r\n  text-overflow: ellipsis;\r\n  height: 100%;\r\n  background-color: rgba(0,0,0,0.7);\r\n  min-height: 100vh;\r\n}\r\n\r\n.menu:hover, .active {\r\n  color: #ff362e;\r\n  padding-left: 15px;\r\n  margin-right: 15px;\r\n}\r\n\r\n#title {\r\n  font-size: 8em;\r\n}\r\n"
+module.exports = "h1 {\r\n  font-family: Merienda;\r\n  font-weight: 400;\r\n  color: #83c1d9;\r\n  -webkit-user-select: none;\r\n     -moz-user-select: none;\r\n      -ms-user-select: none;\r\n          user-select: none;\r\n  text-shadow: 1px 1px #2a4353;\r\n}\r\n\r\na {\r\n  text-decoration: none;\r\n}\r\n\r\n.menu {\r\n  margin: 20px;\r\n  padding-left: 10px;\r\n  font-size: 4em;\r\n  width: 400px;\r\n  background: rgba(169, 169, 236, 0.2);\r\n}\r\n\r\n.main-menu nav{\r\n  margin-top: 50px;\r\n  width: -webkit-fit-content;\r\n  width: -moz-fit-content;\r\n  width: fit-content;\r\n  list-style-type: none;\r\n  white-space: nowrap;\r\n  float: left;\r\n  overflow: hidden;\r\n  text-overflow: ellipsis;\r\n  height: 100%;\r\n  background-color: rgba(0,0,0,0.7);\r\n  min-height: 100vh;\r\n}\r\n\r\n.menu:hover, .active {\r\n  color: #ff362e;\r\n  padding-left: 15px;\r\n  margin-right: 15px;\r\n}\r\n\r\n#title {\r\n  font-size: 8em;\r\n  white-space: nowrap;\r\n}\r\n"
 
 /***/ }),
 
@@ -327,14 +327,14 @@ var HomeComponent = /** @class */ (function () {
 /***/ "./src/app/navbar/navbar.component.css":
 /***/ (function(module, exports) {
 
-module.exports = ".navbar {\r\n  background: transparent;\r\n}\r\n"
+module.exports = ".nav-link {\r\n  text-decoration: none;\r\n  color: white;\r\n}\r\n\r\n.navbar {\r\n  background: transparent;\r\n}\r\n"
 
 /***/ }),
 
 /***/ "./src/app/navbar/navbar.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<nav class=\"navbar navbar-expand-sm bg-light navbar-light fixed-bottom\">\r\n  <ul class=\"navbar-nav\">\r\n    <li class=\"nav-item active\">\r\n      <a class=\"nav-link\" href=\"#\">Active</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link\" href=\"#\">Link</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link\" href=\"#\">Link</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link disabled\" href=\"#\">Disabled</a>\r\n    </li>\r\n  </ul>\r\n</nav>\r\n"
+module.exports = "<nav class=\"navbar navbar-expand-sm fixed-bottom\">\r\n  <ul class=\"navbar-nav\">\r\n    <li class=\"nav-item active\">\r\n      <a class=\"nav-link\" href=\"#\">Active</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link\" href=\"#\">Link</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link\" href=\"#\">Link</a>\r\n    </li>\r\n    <li class=\"nav-item\">\r\n      <a class=\"nav-link disabled\" href=\"#\">Disabled</a>\r\n    </li>\r\n  </ul>\r\n</nav>\r\n"
 
 /***/ }),
 
@@ -377,14 +377,14 @@ var NavbarComponent = /** @class */ (function () {
 /***/ "./src/app/stories/stories.component.css":
 /***/ (function(module, exports) {
 
-module.exports = "\r\n"
+module.exports = ".container {\r\n  background: url('cartoon_scroll.f80e33e567e6cae8e913.png') no-repeat;\r\n  background-size: 600px;\r\n  margin-left: 30px;\r\n  float: left;\r\n  width: 600px;\r\n  height: 100vh;\r\n  text-align: center;\r\n  -webkit-box-sizing: border-box;\r\n          box-sizing: border-box;\r\n  padding: 80px 180px 0 180px;\r\n  font-family: Merienda;\r\n  color: #7d1e0c;\r\n}\r\n"
 
 /***/ }),
 
 /***/ "./src/app/stories/stories.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<app-home></app-home>\n\n<p>Content me with content</p>\n"
+module.exports = "<app-home></app-home>\n\n<div class=\"container\">\n  <p class=\"text-block\">So sorry, turns out theres nothing here yet.</p>\n</div>\n"
 
 /***/ }),
 
