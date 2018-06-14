b90a6f65fc3bb62559579361ca0b6369db972775
jscholar <jinruncai@gmail.com>
2018-06-13 17:10:30 -0700

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -377,16 +377,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var GalleryComponent = /** @class */ (function () {
     function GalleryComponent() {
         this.mockImgs = [
-            '/assets/imgs/Volume%201%20Art/Characters/FELICITY%20SMOKE%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/ICHAIVAL%20DARRYL%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/ICHIZEN%20MICHI%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/KISAI%20JIN%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/KYOI%20FENG.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/SHAN%20SHIGETZU%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/TOMO%20YUKI%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/YUKA.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/ZHA%20TESS%20R1.jpg',
-            '/assets/imgs/Volume%201%20Art/Characters/ZHUYU%20LONG%20R1.jpg'
+            './assets/imgs/Volume%201%20Art/Characters/FELICITY%20SMOKE%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/ICHAIVAL%20DARRYL%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/ICHIZEN%20MICHI%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/KISAI%20JIN%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/KYOI%20FENG.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/SHAN%20SHIGETZU%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/TOMO%20YUKI%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/YUKA.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/ZHA%20TESS%20R1.jpg',
+            './assets/imgs/Volume%201%20Art/Characters/ZHUYU%20LONG%20R1.jpg'
         ];
     }
     GalleryComponent.prototype.ngOnInit = function () {
