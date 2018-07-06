29fc75e55b559d20c7c19e44cab21ee73da13506
jscholar <jinruncai@gmail.com>
2018-07-06 16:33:11 -0700

    Story component accesses param route data. Template uses innerHtml directive to display html

--- src/app/components/story/story.component.ts
+++ src/app/components/story/story.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import { StoryService } from '../../services/story.service';
 
@@ -9,7 +9,9 @@ import { StoryService } from '../../services/story.service';
   styleUrls: ['./story.component.css']
 })
 export class StoryComponent implements OnInit {
-  story: string;
+  private story: string;
+  private chapter: string;
+  public htmlText: string;
   constructor(
     private route: ActivatedRoute,
     private location: Location,
@@ -17,9 +19,21 @@ export class StoryComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      this.story = paramMap.get('story');
+      if (paramMap.has('chapter')) {
+        this.chapter = paramMap.get('chapter');
+      } else {
+        this.chapter = null;
+      }
+    });
   }
   // Sets the selected story
   getStory(): void {
+    this.storyService.getStory().subscribe((data: any) => {
+      this.htmlText = data.content.rendered;
+      console.log(this.htmlText);
+    });
   }
 
 }
