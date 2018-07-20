27b6c0f5a49c96a0bf2bc2669326309eea8d5a77
jscholar <jinruncai@gmail.com>
2018-07-19 17:41:26 -0700

    Changed to Heroku Server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://lantern-dragon-v2-server.ffec474y37.us-west-2.elasticbeanstalk.com'
+  apiUrl: 'https://lantern-dragon-v2-server.herokuapp.com/'
 };
