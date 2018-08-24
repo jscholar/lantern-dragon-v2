cd39dcaf31ec31e592e6ea66c467183ae938c30a
jscholar <jinruncai@gmail.com>
2018-08-23 18:23:52 -0700

    Added select function for characters

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CAST } from '../../shared/constants/cast';
+import { Character } from '../../shared/models/character';
 
 @Component({
   selector: 'app-bio',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bio.component.css']
 })
 export class BioComponent implements OnInit {
-
+  cast = CAST;
+  selected: Character;
   constructor() { }
 
   ngOnInit() {
   }
-
+  select(selected) {
+    this.selected = selected;
+  }
 }
