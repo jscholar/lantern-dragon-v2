c944aab0bb00b0568431c32601edaddc548fef80
jscholar <jinruncai@gmail.com>
2018-08-23 18:23:21 -0700

    Created CAST constant for list of characters

---
+++ src/app/shared/constants/cast.ts
@@ -0,0 +1,31 @@
+import { Character } from '../models/character';
+
+export const CAST: Character[] = [
+  {
+    name: 'Tomo Yuki',
+    portraitUrl: null,
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Zhuyu Long',
+    portraitUrl: null,
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Felicity Smoke',
+    portraitUrl: null,
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  }
+];
---
+++ src/app/shared/models/character.ts
@@ -0,0 +1,9 @@
+export class Character {
+  name: string;
+  portraitUrl: string;
+  age: string;
+  height: string;
+  weight: string;
+  powers: string;
+  description: string;
+}
