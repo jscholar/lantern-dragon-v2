2d8da0ac4e186756c333488d08d7eb8653db9d33
jscholar <jinruncai@gmail.com>
2018-08-31 22:25:28 -0700

    Added characters to gallery.

--- src/app/shared/constants/cast.ts
+++ src/app/shared/constants/cast.ts
@@ -20,8 +20,62 @@ export const CAST: Character[] = [
     description: '??'
   },
   {
+    name: 'Michi Ichizen',
+    portraitUrl: './assets/heroesArt/michi/ichizen_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
     name: 'Felicity Smoke',
     portraitUrl: './assets/heroesArt/felicity/felicity_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Shan Shigetzu',
+    portraitUrl: './assets/heroesArt/shan/shan_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Yuka',
+    portraitUrl: './assets/heroesArt/yuka/yuka_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Ichaival Darryl',
+    portraitUrl: './assets/heroesArt/ichaival/ichaival_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Kisai Jin',
+    portraitUrl: './assets/heroesArt/kisai/kisai_test_color.png',
+    age: '??',
+    height: '??',
+    weight: '??',
+    powers: '??',
+    description: '??'
+  },
+  {
+    name: 'Kyoi Feng',
+    portraitUrl: './assets/heroesArt/kyoi/kyoi_WIP.png',
     age: '??',
     height: '??',
     weight: '??',
