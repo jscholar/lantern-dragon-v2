e697ebd25ea59fccc5f9c4fc54ba8f9683f2ab9f
jscholar <jinruncai@gmail.com>
2018-08-31 22:54:50 -0700

    Added descriptions to bios. Reduced size of portraits

--- src/app/shared/constants/cast.ts
+++ src/app/shared/constants/cast.ts
@@ -8,7 +8,11 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'I’m a freshman attending a local university. Because of\n' +
+    'Ichizen, I often am irritated and prone to hitting him when he\n' +
+    'spouts dumb ideas. Being friends with him for so long has taught\n' +
+    'me that I can expect either a really terrible experience or he’ll get\n' +
+    'hurt one way or another.'
   },
   {
     name: 'Zhuyu Long',
@@ -17,7 +21,10 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Pretty serious person, probably doesn’t smile too much.\n' +
+    'Might have a dislike for Felicity despite her addressing him as\n' +
+    'a friend based on initial experience. Apparently a pretty serious\n' +
+    'student too. Friends with Kisai and Ichaival.'
   },
   {
     name: 'Michi Ichizen',
@@ -25,8 +32,16 @@ export const CAST: Character[] = [
     age: '??',
     height: '??',
     weight: '??',
-    powers: '??',
-    description: '??'
+    powers: 'None',
+    description: 'I’ve known this idiot since elementary school, maybe even\n' +
+    'preschool, it seems like I’ve been with him for that long. He’s\n' +
+    'careless, selfish, and always looking for some kind of adventure.\n' +
+    'Still, I have to admit that he’s always putting himself out there,\n' +
+    'taking on anything. He has a lot of courage but isn’t too bright in\n' +
+    'most situations. But maybe that’s a redeeming quality. Not a bad\n' +
+    'person to hang out with except he really gets on my nervces. Why\n' +
+    'do I still get dragged into his messes? Never a dull time when\n' +
+    'Ichizen’s around.\n'
   },
   {
     name: 'Felicity Smoke',
@@ -34,8 +49,14 @@ export const CAST: Character[] = [
     age: '??',
     height: '??',
     weight: '??',
-    powers: '??',
-    description: '??'
+    powers: 'None',
+    description: 'She seems to know a lot about what Ichizen is investigating\n' +
+    'but her sources are less than trustworthy. Knows Yuka through a\n' +
+    'mutual class. Judging from her clothing, maybe more than middle\n' +
+    'class? Introduced me to her friends who gave us information as\n' +
+    'well. Has a little sister who attends the high school she went\n' +
+    'to. Looks like the type to know a bunch of people and have\n' +
+    'connections'
   },
   {
     name: 'Shan Shigetzu',
@@ -44,7 +65,9 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Tall, compared to everyone else. Can’t really say much\n' +
+    'more since I only saw him talking to Zhuyu about something.\n' +
+    'Wonder if he knows anything about what Ichizen wants to find?'
   },
   {
     name: 'Yuka',
@@ -53,7 +76,10 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Friends since middle school. Always a good person to talk\n' +
+    'to. Apparently knows Felicity through one of her classes. Avid\n' +
+    'bookworm, I recall her always going to the library during middle\n' +
+    'school and high school.'
   },
   {
     name: 'Ichaival Darryl',
@@ -62,7 +88,9 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Pretty plain, can’t exactly say more than that. Looks to be\n' +
+    'the one that friends harass but not in a malicious way. Seems to\n' +
+    'good friend with Zhuyu and Kisai.'
   },
   {
     name: 'Kisai Jin',
@@ -71,7 +99,10 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Upbeat guy, seems like a nice person and easy to talk to.\n' +
+    'Seems like he usually makes things fun but gives off that lazy\n' +
+    'vibe. Can’t say for sure though. Good friends with Zhuyu and\n' +
+    'Ichaival apparently.'
   },
   {
     name: 'Kyoi Feng',
@@ -80,6 +111,7 @@ export const CAST: Character[] = [
     height: '??',
     weight: '??',
     powers: '??',
-    description: '??'
+    description: 'Possible maniac. Damn good with swords. Don’t want to\n' +
+    'piss her off anytime soon'
   }
 ];
